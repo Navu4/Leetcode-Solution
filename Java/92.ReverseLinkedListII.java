a1a5005d9c4510c821647c891fdc1d6dd3d70741
Navu4 <51900952+Navu4@users.noreply.github.com>
2021-05-27 21:07:52 +0530

    update

--- Java/92.ReverseLinkedListII.java
+++ Java/92.ReverseLinkedListII.java
@@ -9,8 +9,8 @@
  * }
  */
 class Solution {
-    public ListNode reverseLinkedList(ListNode head){
-    	ListNode prev = null;
+    public ListNode reverseLinkedList(ListNode head, ListNode after){
+    	ListNode prev = after;
     	ListNode curr = head;
 
     	while(curr != null){
@@ -24,55 +24,87 @@ class Solution {
 
     	return prev;
     }
+    
 
-    public ListNode midNode(ListNode head){
-    	ListNode fast = head;
-    	ListNode slow = head;
-
-    	while(fast.next != null && fast.next.next){
-    		fast = fast.next.next;
-    		slow = slow.next;
-    	}
-
-    	return slow;
-    }
-
+    // My Solution 
     public ListNode reverseBetween(ListNode head, int left, int right) {
+    	if(head == null || head.next == null || left == right)
+    		return head;
+        ListNode dummyHead = new ListNode(0, head);
+        ListNode c = dummyHead;
+
         ListNode leftNode = null;
         ListNode rightNode = null;
+        ListNode prev = null;
+        ListNode after = null;
 
-        ListNode curr = head;
-
-        boolean lbool = true;
-        boolean rbool = true;
-        while(curr != null){
-        	if(curr.val == left && lbool){
-        		leftNode = curr;
-        		lbool = false;
-        	} else if(curr.val == right && rbool){
-        		rightNode = curr;
-        		rbool = false;
+        int idx = 0;
+        while(c != null){
+        	if(idx + 1 == left){
+        		prev = c;
+        		leftNode = c.next;
+                System.out.println(c.val);
+        	}else if(idx == right){
+        		rightNode = c;
         	}
-        	curr = curr.next;
+        	idx++;
+        	c = c.next;
         }
 
-        ListNode dummy = rightNode.next;
-        rightNode.next = null;
+        if(rightNode != null){
+        	after = rightNode.next;
+        	rightNode.next = null;
+        }
+        
 
-        ListNode mid = midNode(leftNode);
-        ListNode newHead = reverseLinkedList(mid);
-        ListNode tHead = newHead;
-        while(leftNode != null && newHead != null){
-        	int temp = leftNode.val;
-        	leftNode.val = newHead.val;
-        	newHead.val = temp;
+        ListNode newHead = reverseLinkedList(leftNode, after);
+        prev.next = newHead;
+       
+        return dummyHead.next;
+    }
 
-        	leftNode = leftNode.next;
-        	newHead = newHead.next;
+
+    // Discussion
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || head.next == null || left == right)
+            return head;   
+        
+        ListNode h = null;  // temp head
+        ListNode t = null;  // tail, temp
+        // make life easier use dummy node
+        ListNode p = new ListNode(0);
+        p.next = head;
+        head = p;
+
+        while (p != null && (h == null || t == null) )
+        {
+            // node before left 
+            if (left == 1)
+                h = p;
+
+            // right node
+            if (right == 0)
+                t = p;  
+
+            left--;
+            right--;
+            p = p.next;
         }
 
-        rightNode.next = reverseLinkedList(tHead);
+        // pop sublist and splice ends
+        p = h.next;         // start of sublist
+        h.next = t.next;    // splice ends
+        t.next = null;      // terminate sublist
 
-        return head;
+        // push sublist items back
+        while (p != null)
+        {
+            t = p.next;     // temp
+            p.next = h.next;
+            h.next = p;
+            p = t;          // next node                
+        }                   
+               
+        return head.next;
     }
 }
